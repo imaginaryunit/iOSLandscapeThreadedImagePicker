453433ec909c02ca7cc2a51701ef39ca88621d64
Jim Huang <jim@napkkin.com>
2012-02-06 01:29:03 -0800

    iOSLandscapeThreadedImagePicker v1.0

--- iOSLandscapeThreadedImagePicker/iOSLandscapeThreadedImagePickerViewController.h
+++ iOSLandscapeThreadedImagePicker/iOSLandscapeThreadedImagePickerViewController.h
@@ -1,13 +1,34 @@
 //
-//  iOSLandscapeThreadedImagePickerViewController.h
-//  iOSLandscapeThreadedImagePicker
+//  iOSLandscapeImagePickerViewController.h
+//  iOSLandscapeImagePicker
 //
-//  Created by Jim Huang on 2/6/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Jim Huang on 2/5/12.
+//  Copyright (c) 2012 Napkkin Inc. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface iOSLandscapeThreadedImagePickerViewController : UIViewController
+@interface iOSLandscapeThreadedImagePickerViewController : UIViewController<UIScrollViewDelegate>
+{
+    
+    NSMutableArray *assets;
+    NSMutableArray *buttons;
+    ALAsset *clickedasset;
+    int block, oldblock, bigblock, oldbigblock;
+    float y;
+}
+@property (weak, atomic) IBOutlet UIScrollView *scrollview;
+@property (strong, nonatomic) ALAsset *clickedasset;
+@property (strong, atomic) NSMutableArray *assets;
+@property (strong, atomic) NSMutableArray *buttons;
+
+-(void)showAssets:(int)start;
+-(IBAction)imageClicked:(id)sender;
+-(void)showAssetsBlock:(NSNumber *)ix;
+-(void)removeAssetsBlockBefore:(int)i;
+-(void)removeAssetsBlockAfter:(int)i;
+-(ALAssetsLibrary *)defaultAssetsLibrary;
 
 @end
--- iOSLandscapeThreadedImagePicker/iOSLandscapeThreadedImagePickerViewController.m
+++ iOSLandscapeThreadedImagePicker/iOSLandscapeThreadedImagePickerViewController.m
@@ -1,60 +1,208 @@
 //
-//  iOSLandscapeThreadedImagePickerViewController.m
-//  iOSLandscapeThreadedImagePicker
+//  iOSLandscapeImagePickerViewController.m
+//  iOSLandscapeImagePicker
 //
-//  Created by Jim Huang on 2/6/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Jim Huang on 2/5/12.
+//  Copyright (c) 2012 Napkkin Inc. All rights reserved.
 //
 
 #import "iOSLandscapeThreadedImagePickerViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @implementation iOSLandscapeThreadedImagePickerViewController
 
-- (void)didReceiveMemoryWarning
+@synthesize scrollview;
+@synthesize clickedasset;
+@synthesize buttons;
+@synthesize assets;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
 
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
+- (void)didReceiveMemoryWarning
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
 }
 
 - (void)viewDidUnload
 {
+    [self setScrollview:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
 
-- (void)viewWillAppear:(BOOL)animated
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    [super viewWillAppear:animated];
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
+    
+    NSLog(@"View did appear");
+    
 }
 
-- (void)viewWillDisappear:(BOOL)animated
+- (void)viewDidLoad
 {
-	[super viewWillDisappear:animated];
+    [super viewDidLoad];
+    
+    UIAlertView *mAlert = [[UIAlertView alloc] initWithTitle:@"Loaded" message:@"Loaded" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    
+    [mAlert show];
+    
+    
+    self.navigationItem.prompt = @"Choose a picture";    
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlackOpaque;
+    
+    
+    scrollview.delegate = self;
+    oldblock = 0;
+    [self.navigationController setNavigationBarHidden:NO];
+    void (^assetEnumerator)(ALAsset *, NSUInteger, BOOL *) = ^(ALAsset *result, NSUInteger index, BOOL *stop) 
+    { 
+        if(result != NULL) 
+        { 
+            [assets addObject:result]; 
+        } 
+    };
+    
+    void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) =  ^(ALAssetsGroup *group, BOOL *stop) 
+    {
+        if(group != nil)
+        {
+            [group enumerateAssetsUsingBlock:assetEnumerator];
+            
+        }
+        [self showAssets:0];
+    };
+    
+    //Fetch photo assets from group album
+    assets = [[NSMutableArray alloc] init];
+    ALAssetsLibrary *library = [self defaultAssetsLibrary];
+    [library enumerateGroupsWithTypes:ALAssetsGroupAlbum usingBlock:assetGroupEnumerator 
+                         failureBlock: ^(NSError *error) { NSLog(@"Failure");}];
 }
 
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
+- (ALAssetsLibrary *)defaultAssetsLibrary {
+    static dispatch_once_t pred = 0;
+    static ALAssetsLibrary *library = nil;
+    dispatch_once(&pred, ^{
+        library = [[ALAssetsLibrary alloc] init];
+    });
+    return library; 
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+//Show images in blocks of 24
+-(void)showAssets:(int)start{
+    
+    if([assets count]>start+24){        
+        for(int i=start;i<=start+24;i=i+6){        
+            [self performSelectorOnMainThread:@selector(showAssetsBlock:)  withObject:[NSNumber numberWithInt:i]   waitUntilDone:NO];        
+        }    
+    }else{
+        for(int i=start;i<start+[assets count];i=i+6){        
+            [self performSelectorOnMainThread:@selector(showAssetsBlock:)  withObject:[NSNumber numberWithInt:i]   waitUntilDone:NO];        
+        }                    
+    }
+    scrollview.contentSize = CGSizeMake(480,(([assets count]/4)+1)*300);    
+}
+
+
+-(void)showAssetsBlock:(NSNumber *)ix{
+    
+    NSRange range = NSMakeRange([ix intValue],6);
+    for (int i = range.location; i<MIN(range.location + range.length, [assets count]); i++) 
+    {
+        UIButton *imgBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [imgBtn setFrame:CGRectMake((i%6*80)+2,(i/6*80)+2,75,75)];
+        imgBtn.tag=i;
+        ALAsset *asset=[assets objectAtIndex:i];
+        [imgBtn addTarget:self action:@selector(imageClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [imgBtn setImage:[UIImage imageWithCGImage:[asset thumbnail]] forState:UIControlStateNormal];
+        [scrollview addSubview:imgBtn];
+        
+    }
+    
+}
+
+//Clear blocks of images outside of current view
+-(void)removeAssetsBlockBefore:(int) i{
+    for (UIButton *btn in [scrollview subviews]) 
+    {
+        if(btn.tag<i)
+            [btn removeFromSuperview];
+    }    
+}
+-(void)removeAssetsBlockAfter:(int) i{
+    for (UIButton *btn in [scrollview subviews]) 
+    {
+        if(btn.tag>i)
+            [btn removeFromSuperview];
+    }    
+}
+
+
+-(IBAction)imageClicked:(id)sender{
+    
+    UIButton *clickedbutton = (UIButton*)sender;        
+    int i = clickedbutton.tag;
+    
+    NSString *msg = [NSString stringWithFormat:@"Clicked on image: %d",i];
+    UIAlertView *mAlert = [[UIAlertView alloc] initWithTitle:@"Image selected!" message:msg delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    
+    [mAlert show];
+    
+    
+}
+
+//Handle scrolling of view
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    block = ((int) scrollView.contentOffset.y)/300;    
+    if(block!=oldblock)
+    {
+        if([[scrollview subviews] count]>2048)
+        {
+            [self removeAssetsBlockBefore:MAX(0,block*24-1024)];
+            [self removeAssetsBlockAfter:MIN([assets count]-7,block*24+1024)];        
+        }        
+        
+        [self showAssets:block*24];
+        
+        oldblock = block;
+    }    
+    
+}
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset{
+    //Each block of images contains 24 images and has height 300    
+    block = ((int) targetContentOffset->y)/300;    
+    if(block!=oldblock)
+    {        
+        [self showAssets:block*24];
+        oldblock = block;
+    }    
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+    //Each block of images contains 24 images and has height 300
+    block = ((int) scrollView.contentOffset.y)/300;    
+    if(block!=oldblock)
+    {
+        [self showAssets:block*24];
+        oldblock = block;
+    }        
 }
 
 @end
